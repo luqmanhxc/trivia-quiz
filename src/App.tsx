7819f3fe1534429e7d6cd07b42b71767619d51a5
Luqman Hakim <luqmanhxc@gmail.com>
2023-02-05 04:40:24 +0800

    feat: react router routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { useAppSelector, useAppDispatch } from './app/hooks';
-import { fetchQuiz } from './features/quiz/QuizSlice';
-import './App.css';
-import QuestionCard from './components/QuestionCard';
-import he from 'he';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Quiz from './pages/Quiz';
+import Results from './pages/Results';
 
-function App() {
-    const { loading, quiz, error } = useAppSelector((state) => state.quiz);
-    const score = useAppSelector((state) => state.score.score);
-    const dispatch = useAppDispatch();
-
-    useEffect(() => {
-        dispatch(fetchQuiz());
-    }, []);
-
+const App = () => {
     return (
-        <div>
-            {score.filter((obj) => obj.correct === true).length}
-            {loading && <div>Loading...</div>}
-            {!loading &&
-                quiz.map((question, index) => (
-                    <QuestionCard
-                        key={question.question}
-                        questionId={index + 1}
-                        question={he.decode(question.question)}
-                        incorrectAnswers={question.incorrect_answers}
-                        correctAnswer={question.correct_answer}
-                    />
-                ))}
-        </div>
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/quiz" element={<Quiz />} />
+                <Route path="/results" element={<Results />} />
+            </Routes>
+        </>
     );
-}
+};
 
 export default App;
